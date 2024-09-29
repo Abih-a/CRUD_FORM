69e09cb9c8ac3779eb4c3d42c9862f853a2b5df8
Abih-a <130190164+Abih-a@users.noreply.github.com>
2024-09-29 16:24:42 +0600

    Add files via upload

---
+++ CRUDForm/CRUDForm/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Corrected connection string without the unrecognized keyword
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IES908U;Initial Catalog=CRUDForm;Integrated Security=True");
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("insert into ut values(@id,@name,@age)", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@age", float.Parse(textBox3.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully saved");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IES908U;Initial Catalog=CRUDForm;Integrated Security=True");
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("update ut set name=@name,age=@age where id= @id", con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                cmd.Parameters.AddWithValue("@name", textBox2.Text);
+                cmd.Parameters.AddWithValue("@age", float.Parse(textBox3.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully update");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IES908U;Initial Catalog=CRUDForm;Integrated Security=True");
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("delete ut where id= @id",con);
+                cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Successfully deleted");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-IES908U;Initial Catalog=CRUDForm;Integrated Security=True");
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from ut where id= @id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+        
+            
+        }
+    }
+    
+}
